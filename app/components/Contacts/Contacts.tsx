28325835ce58e59b41b9d3b0449bc33a9cf91af2
MarkSakhar <mark.sahar18@mail.ru>
2023-10-02 00:33:43 +0200

    first commit

--- app/components/Contacts/Contacts.tsx
+++ app/components/Contacts/Contacts.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import styles from './Contacts.module.scss'
 import Title from '@/app/ui/Title/Title'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import InstagramSVG from '../../../images/socialNetworks/instagram.svg'
 import TelegramSVG from '../../../images/socialNetworks/telegram.svg'
 import VKSVG from '../../../images/socialNetworks/vk.svg'
+
+const InstLink = 'https://www.instagram.com/aksinya_777'
+
+const TgLink = 'https://t.me/aksinya_7777'
+
+const VkLink = 'https://vk.com/aks777'
 
 const Contacts = () => {
   return (
@@ -17,29 +24,35 @@ const Contacts = () => {
         
 
         <div className={styles.links}>
-            <div className={styles.SocialBlock}>
-                <div className={styles.SocialNetworkItem}>
-                    <Image src={InstagramSVG} width={30} height={30} alt="Instagram" />
+            <Link href={InstLink} target="_blank">
+                <div className={styles.SocialBlock}>
+                    <div className={styles.SocialNetworkItem}>
+                        <Image src={InstagramSVG} width={30} height={30} alt="Instagram" />
+                    </div>
+
+                    <span className={styles.textLink}>@aksinya_777</span>
                 </div>
+            </Link>
 
-                <span className={styles.textLink}>@aksinya_777</span>
-            </div>
+            <Link href={TgLink} target="_blank">
+                <div className={styles.SocialBlock}>
+                    <div className={styles.SocialNetworkItem}>
+                        <Image src={TelegramSVG} width={30} height={30} alt="Telegram" />
+                    </div>
 
-            <div className={styles.SocialBlock}>
-                <div className={styles.SocialNetworkItem}>
-                    <Image src={TelegramSVG} width={30} height={30} alt="Telegram" />
+                    <span className={styles.textLink}>@aksinya_7777</span>
                 </div>
+            </Link>
 
-                <span className={styles.textLink}>@aksinya_7777</span>
-            </div>
+            <Link href={VkLink} target="_blank">
+                <div className={styles.SocialBlock}>
+                    <div className={styles.SocialNetworkItem}>
+                        <Image src={VKSVG} width={30} height={30} alt="VK" />
+                    </div>
 
-            <div className={styles.SocialBlock}>
-                <div className={styles.SocialNetworkItem}>
-                    <Image src={VKSVG} width={30} height={30} alt="VK" />
+                    <span className={styles.textLink}>@aks777</span>
                 </div>
-
-                <span className={styles.textLink}>@aks777</span>
-            </div>
+            </Link>
 
         </div>
 
--- app/components/MyWorks/MyWorks.tsx
+++ app/components/MyWorks/MyWorks.tsx
@@ -1,25 +1,105 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
 import styles from './MyWorks.module.scss'
 import Title from '@/app/ui/Title/Title'
 
+// Слайдер
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import ImgOne from '../../../images/MyWorks/1.jpg'
+import ImgTwo from '../../../images/MyWorks/2.jpg'
+import ImgThree from '../../../images/MyWorks/3.jpg'
+import ImgFour from '../../../images/MyWorks/4.jpg'
+import ImgFive from '../../../images/MyWorks/5.jpg'
+import ImgSix from '../../../images/MyWorks/6.jpg'
+
 const MyWorks = () => {
 
+  const [slidesPerView, setSlidesPerView] = useState(3);
+  const [slidesPerGroup, setSlidesPerGroup] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1200) {
+        setSlidesPerView(2);
+        setSlidesPerGroup(2);
+      }
+
+      if (window.innerWidth < 800) {
+        setSlidesPerView(1);
+        setSlidesPerGroup(1);
+      }
+
+      if (window.innerWidth > 1200) {
+        setSlidesPerView(3);
+        setSlidesPerGroup(3);
+      }
+    
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className={styles.MyWorksWrapper}>
         <div className={styles.title}>
             <Title name="Мои работы"/>
         </div>
-       
 
         <div className={styles.blocks}>
-            <div className={styles.item}>1</div>
-            <div className={styles.item}>2</div>
-            <div className={styles.item}>3</div>
-            <div className={styles.item}>4</div>
-            <div className={styles.item}>5</div>
-            <div className={styles.item}>6</div>
-        </div>
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            slidesPerView={slidesPerView}
+            slidesPerGroup={slidesPerGroup}
+            spaceBetween={10}
+            centeredSlides 
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            loop={true}
+            className={styles.swiperContainer}
+
+        >
+          <SwiperSlide>
+            <Image src={ImgOne} alt='Аватарка' className={styles.item} width={400} height={250} /> 
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image src={ImgTwo} alt='Аватарка' className={styles.item} width={400} height={250} /> 
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image src={ImgThree} alt='Аватарка' className={styles.item} width={400} height={250} /> 
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image src={ImgFour} alt='Аватарка' className={styles.item} width={400} height={250} /> 
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image src={ImgFive} alt='Аватарка' className={styles.item} width={400} height={250} /> 
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Image src={ImgSix} alt='Аватарка' className={styles.item} width={400} height={250} /> 
+          </SwiperSlide>
+          
+        </Swiper>
+      </div>
+       
     </div>
   )
 }
