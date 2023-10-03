63acb453e83a7d0228889e46d1422e7303107e9d
MarkSakhar <mark.sahar18@mail.ru>
2023-10-03 22:27:43 +0200

    first commit

--- app/components/MyWorks/MyWorks.tsx
+++ app/components/MyWorks/MyWorks.tsx
@@ -108,37 +108,37 @@ const MyWorks = () => {
         <div className={styles.blocks}>
           <Slider {...settings}>
             <div className={styles.item} >
-              <Image src={ImgOneOne} alt='Аватарка' className={styles.item} width={240} height={270} />
+              <Image src={ImgOneOne} alt='Аватарка' className={`${styles.item} object-fit-contain`} />
             </div>
             <div>
-              <Image src={ImgOneTwo} alt='Аватарка' className={styles.item} width={240} height={270} /> 
+              <Image src={ImgOneTwo} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneThree} alt='Аватарка' className={styles.item} width={240} height={270} /> 
+              <Image src={ImgOneThree} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneFour} alt='Аватарка' className={styles.item} width={240} height={270} /> 
+              <Image src={ImgOneFour} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneFive} alt='Аватарка' className={styles.item} width={240} height={270} /> 
+              <Image src={ImgOneFive} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneSix} alt='Аватарка' className={styles.item} width={230} height={270} /> 
+              <Image src={ImgOneSix} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneSeven} alt='Аватарка' className={styles.item} width={300} height={270} /> 
+              <Image src={ImgOneSeven} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneEight} alt='Аватарка' className={styles.item} width={230} height={270} /> 
+              <Image src={ImgOneEight} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneNine} alt='Аватарка' className={styles.item} width={220} height={270} /> 
+              <Image src={ImgOneNine} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneTen} alt='Аватарка' className={styles.item} width={240} height={270} /> 
+              <Image src={ImgOneTen} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
             <div>
-              <Image src={ImgOneEleven} alt='Аватарка' className={styles.item} width={230} height={270} /> 
+              <Image src={ImgOneEleven} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
             </div>
           </Slider>
       </div>
@@ -147,37 +147,37 @@ const MyWorks = () => {
       <div className={styles.blocks}>
         <Slider {...settings}>
           <div className={styles.item} >
-            <Image src={ImgTwoOne} alt='Аватарка' className={styles.item} width={240} height={300} />
+            <Image src={ImgTwoOne} alt='Аватарка' className={`${styles.item} object-fit-contain`} />
           </div>
           <div>
-            <Image src={ImgTwoTwo} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgTwoTwo} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoThree} alt='Аватарка' className={styles.item} width={250} height={300} /> 
+            <Image src={ImgTwoThree} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoFour} alt='Аватарка' className={styles.item} width={260} height={300} /> 
+            <Image src={ImgTwoFour} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoFive} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgTwoFive} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoSix} alt='Аватарка' className={styles.item} width={230} height={300} /> 
+            <Image src={ImgTwoSix} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoSeven} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgTwoSeven} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoEight} alt='Аватарка' className={styles.item} width={250} height={300} /> 
+            <Image src={ImgTwoEight} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoNine} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgTwoNine} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoTen} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgTwoTen} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgTwoEleven} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgTwoEleven} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
         </Slider>
       </div>
@@ -186,37 +186,37 @@ const MyWorks = () => {
       <div className={styles.blocks}>
         <Slider {...settings}>
           <div className={styles.item} >
-            <Image src={ImgThreeOne} alt='Аватарка' className={styles.item} width={250} height={300} />
+            <Image src={ImgThreeOne} alt='Аватарка' className={`${styles.item} object-fit-contain`} />
           </div>
           <div>
-            <Image src={ImgThreeTwo} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgThreeTwo} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeThree} alt='Аватарка' className={styles.item} width={270} height={300} /> 
+            <Image src={ImgThreeThree} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeFour} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgThreeFour} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeFive} alt='Аватарка' className={styles.item} width={240} height={300} /> 
+            <Image src={ImgThreeFive} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeSix} alt='Аватарка' className={styles.item} width={270} height={300} /> 
+            <Image src={ImgThreeSix} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeSeven} alt='Аватарка' className={styles.item} width={290} height={300} /> 
+            <Image src={ImgThreeSeven} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeEight} alt='Аватарка' className={styles.item} width={350} height={300} /> 
+            <Image src={ImgThreeEight} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeNine} alt='Аватарка' className={styles.item} width={320} height={300} /> 
+            <Image src={ImgThreeNine} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeTen} alt='Аватарка' className={styles.item} width={340} height={300} /> 
+            <Image src={ImgThreeTen} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
           <div>
-            <Image src={ImgThreeEleven} alt='Аватарка' className={styles.item} width={330} height={300} /> 
+            <Image src={ImgThreeEleven} alt='Аватарка' className={`${styles.item} object-fit-contain`} /> 
           </div>
         </Slider>
       </div>
